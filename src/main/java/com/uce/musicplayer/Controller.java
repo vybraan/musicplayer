f9e671c723a154e375390a3fd211a1fe33bd21c6
Ailton Bauque <nevskya@hotmail.com>
2022-09-03 19:37:38 +0200

    removing the main python file

---
+++ src/main/java/com/uce/musicplayer/Controller.java
@@ -0,0 +1,53 @@
+package com.uce.musicplayer;
+
+import com.uce.musicplayer.music.Music;
+import com.uce.musicplayer.music.Song;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.skin.ListViewSkin;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+//    @FXML
+//    private HBox playQueueCard;
+//    @FXML
+//    private HBox recentlyPlayedCard;
+//    @FXML
+//    private FlowPane newlyAddedCard;
+//    @FXML
+//    private AnchorPane newlyAddedPane;
+//    @FXML
+//    private VBox hSongView;
+//    @FXML
+//    private ScrollPane hSongScroll;
+
+//    private ArrayList<Music> AllSongs;
+//    private ArrayList<Song> PlayQueue;
+//    private ArrayList<Song> Favourites;
+//    private ArrayList<Song> MostPlayed;
+//    private ArrayList<Song> NewlyAdded;
+//    private ArrayList<Song> RecentlyPlayed;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+}
--- src/main/java/com/uce/musicplayer/Home.java
+++ src/main/java/com/uce/musicplayer/Home.java
@@ -1,32 +1,32 @@
-package com.uce.musicplayer;
-
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
-
-public class Home extends Application {
-    @FXML
-    private ScrollPane home_scroll;
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Music Player");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//package com.uce.musicplayer;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.control.ScrollPane;
+//import javafx.scene.image.Image;
+//import javafx.stage.Stage;
+//import javafx.stage.StageStyle;
+//
+//import java.io.IOException;
+//
+//public class Home extends Application {
+//    @FXML
+//    private ScrollPane home_scroll;
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("home.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        stage.setTitle("Music Player");
+//        stage.setScene(scene);
+//        stage.setResizable(false);
+//        stage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
